49bb1e9cfd4618a89c6cc1aaf91a62ecf0cfcda5
Bharat <bharat@kali.com>
2020-09-30 21:30:13 +0530

    created project with react and added files for home page

---
+++ src/homepage.component.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+
+
+const HomePage = () => (
+    <div className='homepage'>
+        <div className='directory-menu'>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>hats</h1>
+                    <span className='subtitle'>Shop Now</span>
+                </div>
+            </div>
+        </div>
+
+        <div className='directory-menu'>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>jacket</h1>
+                    <span className='subtitle'>Shop Now</span>
+                </div>
+            </div>
+        </div>
+
+        <div className='directory-menu'>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>belt</h1>
+                    <span className='subtitle'>Shop Now</span>
+                </div>
+            </div>
+        </div>
+
+        <div className='directory-menu'>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>men's</h1>
+                    <span className='subtitle'>Shop Now</span>
+                </div>
+            </div>
+        </div>
+
+        <div className='directory-menu'>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>women's</h1>
+                    <span className='subtitle'>Shop Now</span>
+                </div>
+            </div>
+        </div>
+    </div>
+)
+
+export default HomePage
